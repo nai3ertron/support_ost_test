2ab9ed9db075a1b4f2ba1c358c8b93eed712d3b9
sukan sittibamrungsuk <58160500@go.buu.ac.th>
2018-09-04 13:22:23 +0700

    fix question activity

--- support/include/staff/dashboard.inc.php
+++ support/include/staff/dashboard.inc.php
@@ -53,7 +53,7 @@ $plots = $report->getPlotData();
 <div class="clear"></div>
 <div style="margin-bottom:20px; padding-top:5px;">
     <div class="pull-left flush-left">
-        <h2><?php echo __('Question Activity');
+        <h2><?php echo __('Question  Activity');
             ?>&nbsp;<i class="help-tip icon-question-sign" href="#ticket_activity"></i></h2>
     </div>
 </div>

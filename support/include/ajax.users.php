32d14332b9dcc955374795a1d306944431e9442e
sukan sittibamrungsuk <58160500@go.buu.ac.th>
2018-09-14 10:59:38 +0700

    fix auth

--- support/include/ajax.users.php
+++ support/include/ajax.users.php
@@ -85,7 +85,7 @@ class UsersAjaxAPI extends AjaxController {
             if ($emails = array_filter($emails)) {
                 $users->union(User::objects()
                     ->values_flat('id', 'name', 'default_email__address')
-                    ->annotate(array('__relevance__' => new SqlCode(1)))
+                    // ->annotate(array('__relevance__' => new SqlCode(1)))
                     ->filter(array(
                         'emails__address__in' => $emails
                 )));

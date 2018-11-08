1609315d163940c75118bcc110fbe66abbe77736
sukan.s <58160500@go.buu.ac.th>
2018-11-08 11:39:37 +0700

    title + lang

--- support/include/staff/login.header.php
+++ support/include/staff/login.header.php
@@ -7,7 +7,7 @@ header("X-Frame-Options: SAMEORIGIN");
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta http-equiv="refresh" content="7200" />
-    <title>osTicket :: <?php echo __('Agent Login'); ?></title>
+    <title><?php echo __('Helpdesk'); ?></title>
     <link rel="stylesheet" href="css/login.css" type="text/css" />
     <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/font-awesome.min.css?035fd0a"/>
     <meta name="robots" content="noindex" />
